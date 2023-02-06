7eb27e7fa778da67cdc9617c93c392cd65059e04
Saifdevblockchain <saifurehman980@gmail.com>
2023-02-06 19:36:03 +0500

    Legion Graph

--- src/legion-stake.ts
+++ src/legion-stake.ts
@@ -1,3 +1,4 @@
+import { Bytes, BigInt, store } from '@graphprotocol/graph-ts'
 import {
   Claim as ClaimEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
@@ -7,6 +8,7 @@ import {
   WithdrawToken as WithdrawTokenEvent
 } from "../generated/LegionStake/LegionStake"
 import {
+  Data,
   Claim,
   OwnershipTransferred,
   Stake,
@@ -33,26 +35,79 @@ export function handleOwnershipTransferred(
   entity.previousOwner = event.params.previousOwner
   entity.newOwner = event.params.newOwner
   entity.save()
+
 }
 
 export function handleStake(event: StakeEvent): void {
   let entity = new Stake(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    event.params.staker.toHexString()
   )
   entity.staker = event.params.staker
   entity._amount = event.params._amount
   entity._lockPeriod = event.params._lockPeriod
   entity._tier = event.params._tier
   entity.save()
+
+  let id = "Data"
+  let entityToUpdate = Data.load(id)
+  if (entityToUpdate) {
+    let a = entityToUpdate.averageLockPeriod.toI32()
+    let b = event.params._lockPeriod.toI32()
+    entityToUpdate.averageLockPeriod = BigInt.fromI32(a + b)
+    let c = entityToUpdate.totalNumberOfStakes.toI32()
+    entityToUpdate.totalNumberOfStakes = BigInt.fromI32(c + 1)
+    entityToUpdate.save()
+  }
+  else {
+    let entityToCreate = new Data(id)
+    entityToCreate.averageLockPeriod = event.params._lockPeriod
+    entityToCreate.totalNumberOfStakes = BigInt.fromI32(1)
+    entityToCreate.save()
+  }
+  // let arr: Array<string> = new Array();
+  // if(id){
+  //   arr.push(id);
+  // }
+  // for(let i=0; i<arr.length; i++){
+  //   console.log(arr[i].toString());
+  // }
+  // let as = new AverageStake(id);
+
+  // for(let i = 0; i < ){
+
+  // }
+  // as.averageStake= event.params._amount;
+  // as.save();
+
 }
 
 export function handleUnstake(event: UnstakeEvent): void {
   let entity = new Unstake(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    event.params.unstaker.toHexString()
   )
   entity.unstaker = event.params.unstaker
   entity.unstakeTime = event.params.unstakeTime
   entity.save()
+
+  let lockPeriodToConsider = new BigInt(0)
+
+  let id = event.params.unstaker.toHexString()
+  let entityToRemove = Stake.load(id)
+  if (entityToRemove) {
+    lockPeriodToConsider = entityToRemove._lockPeriod
+    store.remove("Stake", id)
+  }
+
+  id = "Data"
+  let entityToUpdate = Data.load(id)
+  if (entityToUpdate) {
+    let a = entityToUpdate.averageLockPeriod.toI32()
+    let b = lockPeriodToConsider.toI32()
+    entityToUpdate.averageLockPeriod = BigInt.fromI32(a - b)
+    let c = entityToUpdate.totalNumberOfStakes.toI32()
+    entityToUpdate.totalNumberOfStakes = BigInt.fromI32(c - 1)
+    entityToUpdate.save()
+  }
 }
 
 export function handleWithdraw(event: WithdrawEvent): void {
